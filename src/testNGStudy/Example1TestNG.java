b4a0281db5b3c1c3f24bf45c24a1c101a217ef96
Nikhil HDK <nikhilhdk9@gmail.com>
2022-07-12 10:11:25 +0530

    My 1st Commit

---
+++ src/testNGStudy/Example1TestNG.java
@@ -0,0 +1,18 @@
+package testNGStudy;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+public class Example1TestNG {
+  @Test
+  public void KiteLogin() throws InterruptedException 
+  {	
+	  	System.setProperty("webdriver.firefox.driver", "C:\\Users\\nikhi\\eclipse-workspace\\TestNGNotes\\geckodriver.exe");
+		WebDriver driver=new FirefoxDriver();
+		driver.manage().window().maximize();
+		driver.get("https://kite.zerodha.com/");
+		Thread.sleep(2000);
+		driver.close();
+  }
+}
